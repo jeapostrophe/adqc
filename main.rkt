052c858fa34eedfd030e0604219a596f6bda709d
Jay McCarthy <jay.mccarthy@gmail.com>
2018-04-23 16:45:24 +0200

    up

--- main.rkt
+++ main.rkt
@@ -1,26 +1,44 @@
 #lang racket/base
 (require racket/contract/base
+         racket/contract/region
          racket/match
          racket/set
+         syntax/parse/define
          (for-syntax racket/base
                      racket/syntax
-                     syntax/parse))
+                     syntax/parse
+                     syntax/parse/experimental/template))
+(module+ test
+  (require chk
+           racket/pretty))
 
-(define-syntax-rule
-  (define-type (P [PF PF/ctc] ...)
-    (V [F F/ctc] ...) ...)
-  (begin (struct P (PF ...))
-         (struct V P (F ...))
-         ...
-         (provide
-          (contract-out
-           (struct P ([PF PF/ctc] ...))
-           (struct (V P) ([PF PF/ctc] ... [F F/ctc] ...))
-           ...))))
-
-;; AST
+;;; Library
+(begin-for-syntax
+  (define (format-ids base-stx fmt base-name vars)
+    (for/list ([v (in-list (syntax->list vars))])
+      (format-id base-stx fmt base-name v)))
+  (define symbol->keyword (compose1 string->keyword symbol->string)))
+(define-syntax (define-interface stx)
+  (syntax-parse stx
+    [(_ x:id ([f:id f/ctc:expr] ...))
+     #:with *x (generate-temporary #'x)
+     #:with x? (format-id #'x "~a?" #'x)
+     #:with *x? (format-id #'*x "~a?" #'*x)
+     #:with (x-f ...) (format-ids #'x "~a-~a" #'x #'(f ...))
+     #:with (*x-f ...) (format-ids #'*x "~a-~a" #'*x #'(f ...))
+     #:with (kw-f ...) (map (compose1 symbol->keyword syntax-e)
+                            (syntax->list #'(f ...)))
+     (template/loc stx
+                   (begin (struct *x (f ...) #:transparent)
+                          (define x? *x?)
+                          (define/contract (x (?@ kw-f f) ...)
+                            (->* ((?@ kw-f f/ctc) ...) x?)
+                            (*x f ...))
+                          (define/contract x-f (-> x? any) *x-f) ...
+                          (provide x? x-f ...)))]))
 
 ;;; Types
+
 (define bitwidths '(1 8 16 32 64))
 (define IntegerBitWidth?
   (apply or/c bitwidths))
@@ -31,25 +49,46 @@
 (define Index?
   exact-nonnegative-integer?)
 (define Field? symbol?)
-(define-type (Type)
-  ;; This is for external data
-  (PtrT)
-  (IntT [signed? boolean?] [w IntegerBitWidth?])
-  (FloT [w FloatBitWidth?])
-  ;; NOTE This means that functions are not polymorphic in the array
-  ;; dimension, so we have to use meta-programming to generate many
-  ;; versions of the same function specialized for different sizes, or
-  ;; we have to slice arrays.
-  (ArrT [dim Dimension?] [elem Type?])
-  (RecT [field->ty (hash/c #:immutable #t Field? Type?)]))
-(define AtomicT?
-  (or/c PtrT? IntT? FloT?))
-(define NumT?
-  (or/c IntT? FloT?))
+
+(define-interface Type
+  ([size exact-nonnegative-integer?]
+   [ctc contract?]
+   [fmt any/c]
+   [Int (or/c #f (cons/c boolean? IntegerBitWidth?))]
+   [Flo (or/c #f FloatBitWidth?)]))
+;; XXX fmt should have a variable to add it to
+
+(define Void
+  (Type #:size 0
+        #:ctc none/c
+        #:fmt #f
+        #:Int #f
+        #:Flo #f))
+
+(define PtrT
+  (Type #:size 64
+        #:ctc none/c
+        #:fmt "(void *)"
+        #:Int #f
+        #:Flo #f))
+
+(define/contract (IntT signed? w)
+  (-> boolean? IntegerBitWidth? Type?)
+  (define W (expt 2 w))
+  (Type #:size w
+        #:ctc
+        (if (= w 1) (or/c 0 1 boolean?)
+            (integer-in (if signed? (* -1 W) 0)
+                        (sub1 W)))
+        #:fmt
+        (if (= w 1) "bool"
+            (format "~aint~a_t" (if signed? "" "u") w))
+
+        #:Int (cons signed? w)
+        #:Flo #f))
 
 (define-syntax-rule (define-Integers [id signed? w] ...)
   (begin (define id (IntT signed? w)) ...))
-
 (define-Integers
   [ U1 #f  1]
   [ U8 #f  8] [ S8 #t  8]
@@ -57,28 +96,155 @@
   [U32 #f 32] [S32 #t 32]
   [U64 #f 64] [S64 #t 64])
 (define Bool U1)
+
+(define/contract (FloT w)
+  (-> FloatBitWidth? Type?)
+  (Type #:size w
+        #:ctc (match w [32 single-flonum?] [64 double-flonum?])
+        #:fmt (match w [32 "float"] [64 "double"])
+        #:Int #f
+        #:Flo w))
+
 (define F32 (FloT 32))
 (define F64 (FloT 64))
 
+;; NOTE This means that functions are not polymorphic in the array
+;; dimension, so we have to use meta-programming to generate many
+;; versions of the same function specialized for different sizes, or
+;; we have to slice arrays.
+(define/contract (ArrT dim elem)
+  (-> Dimension? Type? Type?)
+  (Type #:size (* dim (Type-size elem))
+        #:ctc (and/c vector? (λ (v) (= (vector-length v) dim))
+                     (vectorof (Type-ctc elem)))
+        #:fmt (format "~a[~a]" (Type-fmt elem) dim)
+        #:Int #f
+        #:Flo #f))
+
+(define/contract (RecT field->ty)
+  (-> (hash/c #:immutable #t Field? Type?) Type?)
+  (Type #:size (for/sum ([t (in-hash-values field->ty)])
+                 (Type-size t))
+        #:ctc (and/c hash? (λ (h) (= (hash-count h) (hash-count field->ty)))
+                     (hash/dc
+                      [f (apply or/c (hash-keys field->ty))]
+                      [v (f) (Type-ctc (hash-ref field->ty f))]
+                      #:immutable #t))
+        #:fmt (list "struct {"
+                    (for/list ([(k v) (in-hash field->ty)])
+                      (list (Type-fmt v) " " k ";"))
+                    "}")
+        #:Int #f
+        #:Flo #f))
+
+(define/contract (UniT mode->ty)
+  (-> (hash/c #:immutable #t Field? Type?) Type?)
+  (Type #:size (for/fold ([m 0]) ([t (in-hash-values mode->ty)])
+                 (max m (Type-size t)))
+        #:ctc (and/c hash? (λ (h) (= (hash-count h) (hash-count mode->ty)))
+                     (hash/dc
+                      [f (apply or/c (hash-keys mode->ty))]
+                      [v (f) (Type-ctc (hash-ref mode->ty f))]
+                      #:immutable #t))
+        #:fmt (list "union {"
+                    (for/list ([(k v) (in-hash mode->ty)])
+                      (list (Type-fmt v) " " k ";"))
+                    "}")
+        #:Int #f
+        #:Flo #f))
+
+(provide PtrT
+         U1 Bool U8 S8 U16 S16 U32 S32 U64 S64
+         F32 F64
+         ArrT RecT UniT)
+(module+ test
+  (chk (Type-size (UniT (hash 'x (RecT (hasheq 'a U32 'b S64))
+                              'y (UniT (hasheq 'c S8 'd U16)))))
+       96))
+
+;;; Expression
+
+;;;; Interval Arithmetic
+(struct ival (lo hi) #:transparent)
+(define (iunit x) (ival x x))
+(define (ival+ . xs)
+  (for/fold ([lo 0] [hi 0] #:result (ival lo hi))
+            ([x (in-list xs)])
+    (match-define (ival lox hix) x)
+    (values (+ lo lox) (+ hi hix))))
+;;;; / Interval Arithmetic
+
 (define Variable? symbol?)
 
-;; Helpers
+(define-interface Expr
+  ([type Type?]
+   [unsafe? boolean?]
+   [read-vs (set/c Variable?)]
+   [write-vs (set/c Variable?)]
+   [v->ty (hash/c #:immutable #t Variable? Type?)]
+   [mem Type?]
+   [rtime ival?]))
+;; XXX format (should take a mapping from variable to path)
+;; XXX weakest-precondition
+;; XXX strongest-precondition
 
-(define (integer-fits? ty i)
-  (error 'XXX))
-(define (float-fits? ty f)
-  (error 'XXX))
+(define mt-set (seteq))
+(define mt-map (hasheq))
+(define (map-union x y)
+  (for/fold ([x x]) ([(k v) (in-hash y)])
+    (hash-update x k
+                 (λ (old)
+                   (or (and (not old) v)
+                       (eq? old v)
+                       #; ;; XXX implement Type-union
+                       ((Type-union old) v)
+                       (error 'map-union "Cannot merge map component ~a: ~a vs ~a"
+                              k old v)))
+                 #f)))
 
-(define (type=? x y)
-  (error 'XXX))
+(define/contract (VarR ty x)
+  (-> Type? Variable? Expr?)
+  (Expr #:type ty
+        #:unsafe? #f
+        #:read-vs (seteq x)
+        #:write-vs mt-set
+        #:v->ty (hasheq x ty)
+        #:mem Void
+        #:rtime (iunit 1)))
 
-(define (fail! . args)
-  (error 'XXX))
+(define/contract (Val ty v)
+  (->i ([ty Type?] [v (ty) (Type-ctc ty)]) [r Expr?])
+  (Expr #:type ty
+        #:unsafe? #f
+        #:read-vs mt-set
+        #:write-vs mt-set
+        #:v->ty mt-map
+        #:mem Void
+        #:rtime (iunit 1)))
 
-(define (merge-v->ty x y)
-  (error 'XXX))
+(define (hash-set? h k v)
+  (if v (hash-set h k v) h))
 
-;;; Expressions := Only read from memory, perform no allocation
+(define/contract (Let v #:read-only? [read-only? boolean?]
+                      e be)
+  (->* (Variable? Expr? Expr?) (#:read-only? boolean?) Expr?)
+  (define bs-writes (Expr-write-vs be))
+  (when (and read-only?
+             (set-member? bs-writes v))
+    (error 'Let "Cannot mutate read-only variable: ~a" v))
+  (Expr #:type (Expr-type be)
+        #:unsafe? (or (Expr-unsafe? e) (Expr-unsafe? be))
+        #:read-vs (set-union (Expr-read-vs e)
+                             (set-remove (Expr-read-vs be) v))
+        #:write-vs (set-union (Expr-write-vs e)
+                              (set-remove bs-writes v))
+        #:v->ty (map-union (Expr-v->ty e)
+                           (hash-remove (Expr-v->ty be) v))
+        #:mem
+        (RecT (hasheq 'var (Expr-type e)
+                      'body (UniT (hasheq 'e (Expr-mem e)
+                                          'be (Expr-mem be)))))
+        #:rtime (ival+ (Expr-rtime e) (iunit 1) (Expr-rtime be))))
 
 (define BinOperator?
   (or/c
@@ -93,107 +259,123 @@
    'ffalse 'foeq 'fogt 'foge 'folt 'fole 'fone 'ford
    'ftrue 'fueq 'fuge 'fuge 'fult 'fule 'fune 'funo))
 
-;; XXX It should be possible to take an expression like "x.f" and
-;; "x[5]" and combine them into "x.f + y[5]" by writing something
-;; like:
-#;(Bin 'iadd
-       (RecR (VarR 'x) 'f)
-       (Rename (hasheq 'y 'x)
-               (ArrR (VarR 'x) (IntV 5))))
-;; Perhaps it should also be possible to do something like a "Prefix"
-;; to add a prefix to everything inside... although perhaps that's an
-;; operation that can be written on top of Rename.
-
-(define-type (Expr [ty Type?] [v->ty (hash/c Variable? Type?)])
-  (*IntV [i exact-integer?])
-  (*FloV [f flonum?])
-  (*VarR [x Variable?])
-  (*RecR [r Expr?] [f Field?])
-  ;; NOTE In compiler/verifier, assert that i is within bounds. This
-  ;; means we need to know a's type during the emit/verify process.
-  (*ArrR [a Expr?] [i Expr?])
-  ;; NOTE This is a static slice (so we can predict what the size of
-  ;; the resulting array will be)... and is basically pointer
-  ;; arithmetic.
-  (*ArrS [a Expr?] [s Index?] [e Index?])
-  (*Cast [e Expr?])
-  (*Bin [op BinOperator?] [l Expr?] [r Expr?]))
-
-(define (IntV ty i)
-  (and (and (IntT? ty)
-            (fail! ty "must be integer type"))
-       (or (integer-fits? ty i)
-           (fail! i "doesn't fit in" ty))
-       (*IntV ty (hasheq) i)))
-(define (FloV ty f)
-  (and (and (FloT? ty)
-            (fail! ty "must be float type"))
-       (or (float-fits? ty f)
-           (fail! f "doesn't fit in" ty))
-       (*FloV ty (hasheq) f)))
-(define (VarR ty v)
-  (*VarR ty (hasheq v ty) v))
-(define (RecR r f)
-  (match (Expr-ty r)
-    [(RecT f->t)
-     (define f-ty (hash-ref f->t f #f))
-     (and (or f-ty
-              (fail! r "has no" f "field"))
-          (*RecR f-ty (Expr-v->ty r) r f))]
-    [_ (fail! r "is not a record")]))
-(define (ArrR a i)
-  (match (Expr-ty a)
-    [(ArrT dim e-ty)
-     (*ArrR e-ty (Expr-v->ty a) a i)]
-    [_ (fail! a "is not an array")]))
-(define (ArrS a s e)
-  (and (or (<= s e)
-           (fail! s "is not less than or equal to" e))
-       (match (Expr-ty a)
-         [(ArrT dim e-ty)
-          (and (or (<= s dim)
-                   (fail! a "is" dim "large, not long enough for slice to start at" s))
-               (or (<= e dim)
-                   (fail! a "is" dim "large, not long enough for slice to end at" e))
-               (*ArrS (ArrT (- e s) e-ty) (Expr-v->ty a) a s e))]
-         [_ (fail! a "is not an array")])))
-(define (Cast ty e)
-  (define ety (Expr-ty e))
-  (and (or (NumT? ty)
-           (fail! ty "must be number type"))
-       (or (NumT? ety)
-           (fail! e "is not a number"))
-       (*Cast ty (Expr-v->ty e) e)))
-(define (Bin op l r)
-  (define lt (Expr-ty l))
-  (define rt (Expr-ty r))
-  (define v->ty (merge-v->ty (Expr-v->ty l) (Expr-v->ty r)))
-  (define-values (Which? Result)
+(define/contract (Bin op lhs rhs)
+  (-> BinOperator? Expr? Expr? Expr?)
+  (define lhs-ty (Expr-type lhs))
+  (define rhs-ty (Expr-type rhs))
+  (define-values (Type-cmp result-ty)
     (match op
       [(or 'iadd 'isub 'imul 'iudiv 'isdiv 'iurem 'isrem
            'ishl 'ilshr 'iashr 'iand 'ior 'ixor)
-       (values IntT? lt)]
+       (values Type-Int lhs-ty)]
       [(or 'fadd 'fsub 'fmul 'fdiv 'frem)
-       (values FloT? lt)]
+       (values Type-Flo lhs-ty)]
       [(or 'ieq 'ine 'iugt 'iuge 'iult 'iule 'isgt 'isge 'islt 'isle)
-       (values IntT? Bool)]
+       (values Type-Int Bool)]
       [(or 'ffalse 'foeq 'fogt 'foge 'folt 'fole 'fone 'ford
            'ftrue 'fueq 'fuge 'fuge 'fult 'fule 'fune 'funo)
-       (values FloT? Bool)]))
-  (and (or (type=? lt rt)
-           (fail! lt "and" rt "do not have same type for binary" op))
-       (or (Which? lt)
-           (fail! l "has wrong type," lt ", should be" Which?))
-       (*Bin Result v->ty op l r)))
+       (values Type-Flo Bool)]))
+  (unless (and (Type-cmp lhs-ty)
+               (Type-cmp rhs-ty)
+               (equal? lhs-ty rhs-ty))
+    (error 'Bin "Cannot perform ~a on ~a and ~a, types are wrong ~a and ~a"
+           op lhs rhs lhs-ty rhs-ty))
+  (Expr #:type result-ty
+        #:unsafe? (or (Expr-unsafe? lhs) (Expr-unsafe? rhs))
+        #:read-vs (set-union (Expr-read-vs lhs) (Expr-read-vs rhs))
+        #:write-vs (set-union (Expr-write-vs lhs) (Expr-write-vs rhs))
+        #:v->ty (map-union (Expr-v->ty lhs) (Expr-v->ty rhs))
+        #:mem (UniT (hasheq 'lhs (Expr-mem lhs) 'rhs (Expr-mem rhs)))
+        #:rtime (ival+ (Expr-rtime lhs) (Expr-rtime rhs) (iunit 1))))
 
-(define (BoolV b)
-  (IntV Bool (if b 1 0)))
+;; XXX ArrR
+;; XXX RecR
+;; XXX UniR
 
-;;; Statements := May write to memory
+;; XXX Unsafe
+;; XXX Cast
 
-#;(RecV [list-of-field*exp (listof (cons/c Field? Expr?))])
-#;(ArrV [vs (listof Expr?)])
+;; XXX Assert
+;; XXX Assign
+;; XXX Seq
+;; XXX If
+;; XXX Loop
+;; XXX Break
+;; XXX Continue
 
-;;; Programs := Fix a memory rep and gives names
+(module+ test
+  (pretty-print
+   (Let 'x (Val U32 6)
+        (Let 'y (Val F64 3.14)
+             (Bin 'iadd (VarR U32 'x) (Val U32 8))))))
 
-;; /AST
+;; XXX
+
+#;(
+
+   ;;; Expressions := Only read from memory, perform no allocation
+
+   ;; XXX It should be possible to take an expression like "x.f" and
+   ;; "x[5]" and combine them into "x.f + y[5]" by writing something
+   ;; like:
+   #;(Bin 'iadd
+          (RecR (VarR 'x) 'f)
+          (Rename (hasheq 'y 'x)
+                  (ArrR (VarR 'x) (IntV 5))))
+   ;; Perhaps it should also be possible to do something like a "Prefix"
+   ;; to add a prefix to everything inside... although perhaps that's an
+   ;; operation that can be written on top of Rename.
+
+   (define-type (Expr [ty Type?] [v->ty (hash/c Variable? Type?)])
+     
+     (*RecR [r Expr?] [f Field?])
+     ;; NOTE In compiler/verifier, assert that i is within bounds. This
+     ;; means we need to know a's type during the emit/verify process.
+     (*ArrR [a Expr?] [i Expr?])
+     ;; NOTE This is a static slice (so we can predict what the size of
+     ;; the resulting array will be)... and is basically pointer
+     ;; arithmetic.
+     (*ArrS [a Expr?] [s Index?] [e Index?])
+     (*Cast [e Expr?]))
+
+   (define (RecR r f)
+     (match (Expr-ty r)
+       [(RecT f->t)
+        (define f-ty (hash-ref f->t f #f))
+        (and (or f-ty
+                 (fail! r "has no" f "field"))
+             (*RecR f-ty (Expr-v->ty r) r f))]
+       [_ (fail! r "is not a record")]))
+   (define (ArrR a i)
+     (match (Expr-ty a)
+       [(ArrT dim e-ty)
+        (*ArrR e-ty (Expr-v->ty a) a i)]
+       [_ (fail! a "is not an array")]))
+   (define (ArrS a s e)
+     (and (or (<= s e)
+              (fail! s "is not less than or equal to" e))
+          (match (Expr-ty a)
+            [(ArrT dim e-ty)
+             (and (or (<= s dim)
+                      (fail! a "is" dim "large, not long enough for slice to start at" s))
+                  (or (<= e dim)
+                      (fail! a "is" dim "large, not long enough for slice to end at" e))
+                  (*ArrS (ArrT (- e s) e-ty) (Expr-v->ty a) a s e))]
+            [_ (fail! a "is not an array")])))
+   (define (Cast ty e)
+     (define ety (Expr-ty e))
+     (and (or (NumT? ty)
+              (fail! ty "must be number type"))
+          (or (NumT? ety)
+              (fail! e "is not a number"))
+          (*Cast ty (Expr-v->ty e) e)))
+
+   ;;; Statements := May write to memory
+
+   #;(RecV [list-of-field*exp (listof (cons/c Field? Expr?))])
+   #;(ArrV [vs (listof Expr?)])
+
+   ;;; Programs := Fix a memory rep and gives names
+
+   ;; /AST
+   )
