78043308d98f396c5f6b4d56bcec951556eee595
Jay McCarthy <jay.mccarthy@gmail.com>
2018-06-04 13:57:17 -0400

    tree-for

--- compile.rkt
+++ compile.rkt
@@ -1,12 +1,11 @@
 #lang racket/base
 (require racket/format
+         racket/list
          racket/match
          "grammar.rkt")
 
 (define (bit-width->cast signed? bw)
-  (format "(~aint~a_t)"
-          (if signed? "" "u")
-          bw))
+  (list* "(" (if signed? "" "u") "int" (~a bw) "_t)"))
 
 (define bin-op-table
   (hasheq 'iadd "+"
@@ -18,37 +17,30 @@
 (define (compile-expr exp)
   (match exp
     [(Integer signed? bits val)
-     (format "(~a~a)" (bit-width->cast signed? bits) val)]
+     (list* "(" (bit-width->cast signed? bits) (~a val) ")")]
     [(Variable name)
+     ;; XXX something to translate
      (~a name)]
     [(IBinOp op L R)
      (define op-str (hash-ref bin-op-table op))
      (define L-str (compile-expr L))
      (define R-str (compile-expr R))
-     (format "(~a ~a ~a)" L-str op-str R-str)]))
+     (list* L-str " " op-str " " R-str)]))
 
 (define (compile-stmt stmt)
   (match stmt
     [(Skip)
-     "// Skip"]
+     empty]
     [(Assign dest exp)
-     (format "~a = (~a);" dest (compile-expr exp))]
+     (list* dest " = " (compile-expr exp) ";")]
     [(Begin L-stmt R-stmt)
-     (format "~a~n~a" (compile-stmt L-stmt) (compile-stmt R-stmt))]
+     (list* (compile-stmt L-stmt) ind-nl (compile-stmt R-stmt))]
     [(If pred then else)
-     (list (format "if (~a) {" (compile-expr pred))
-           ind++
-           ind-nl
-           (compile-stmt then)
-           ind--
-           ind-nl
-           "} else {"
-           ind++
-           ind-nl
-           (compile-stmt else)
-           ind--
-           ind-nl
-           "}")]
+     (list* "if " (compile-expr pred) " {" ind++ ind-nl
+            (compile-stmt then)
+            ind-- ind-nl "} else {" ind++ ind-nl
+            (compile-stmt else)
+            ind-- ind-nl "}")]
     [(While pred invar do-stmt)
      (error "TODO: While stmt")]))
 
@@ -72,8 +64,14 @@
      (set-box! ind-lvl (- (unbox ind-lvl) 2))]
     [_ (display v)]))
 
+(define (tree-for f t)
+  (match t
+    [(or (? void?) #f '()) (void)]
+    [(cons a d) (tree-for f a) (tree-for f d)]
+    [x (f x)]))
+
 (module+ test
-  (for-each idisplay
+  (tree-for idisplay
             (compile-stmt (If (IEq (i32 5) (i32 6))
                               (Assign 'x (i32 1))
                               (Assign 'y (i32 2))))))
