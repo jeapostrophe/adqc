930d59f043db702bab3cd7f931c29439936a0253
Jay McCarthy <jay.mccarthy@gmail.com>
2018-06-07 21:02:24 -0400

    check

--- stx.rkt
+++ stx.rkt
@@ -3,6 +3,7 @@
          racket/list
          racket/match
          syntax/parse/define
+         racket/stxparam
          "ast.rkt")
 
 ;; XXX This module should use plus not ast (i.e. the thing that does
@@ -129,6 +130,7 @@
 
 (require (for-syntax racket/base
                      syntax/parse
+                     racket/syntax
                      racket/dict
                      syntax/id-table))
 
@@ -172,6 +174,9 @@
   S-free-macros define-S-free-syntax
   S-expander define-S-expander)
 
+(define-syntax-parameter current-return #f)
+(define-syntax-parameter current-return-var #f)
+
 (define-syntax (while stx) (raise-syntax-error 'while "Illegal outside S" stx))
 (define-syntax (S stx)
   (syntax-parse stx
@@ -181,8 +186,8 @@
     [(_ (error m)) (syntax/loc stx (Fail m))]
     [(_ (begin)) (syntax/loc stx (S (void)))]
     [(_ (begin (define . d) . b)) (syntax/loc stx (S (let (d) . b)))]
-    [(_ (begin s)) (syntax/loc stx (S s))]    
-    [(_ (begin a . d)) (syntax/loc stx (Begin (S a) (S (begin . d))))]    
+    [(_ (begin s)) (syntax/loc stx (S s))]
+    [(_ (begin a . d)) (syntax/loc stx (Begin (S a) (S (begin . d))))]
     [(_ (set! p e)) (syntax/loc stx (Assign (P p) (E e)))]
     [(_ (if p t f)) (syntax/loc stx (If (E p) (S t) (S f)))]
     [(_ (let/ec k:id . b))
@@ -190,8 +195,7 @@
        (let ([k-id (gensym 'k)])
          (Let/ec k-id
                  (let ([the-ret (Jump k-id)])
-                   (let-syntax ([k (S-expander
-                                    (syntax-parser [(_) #'the-ret]))])
+                   (let-syntax ([k (S-expander (syntax-parser [(_) #'the-ret]))])
                      (S (begin . b)))))))]
     [(_ (while (~optional (~seq #:I I)
                           #:defaults ([I #'(U32 1)]))
@@ -257,12 +261,113 @@
               (void (format "Checked: ~a" p-msg))
               (error (format "Failed! ~a" p-msg))))))]))
 
-;; XXX implement F
+(begin-for-syntax
+  (define-syntax-class Farg
+    #:attributes (x ref var arg)
+    #:description "function argument"
+    [pattern
+     ((~optional (~and #:const (~bind [mode #''copy]))
+                 #:defaults ([mode #''read-only]))
+      x:id (~datum :) ty)
+     #:attr ref (generate-temporary #'x)
+     #:attr var (syntax/loc this-syntax (Var (gensym 'x) ty))
+     #:attr arg (syntax/loc this-syntax (Arg (Var-x ref) (Var-ty ref) mode))])
+  (define-syntax-class Fret
+    #:attributes (x ref var)
+    #:description "function return"
+    [pattern
+     (x:id (~datum :) ty)
+     #:attr ref (generate-temporary #'x)
+     #:attr var (syntax/loc this-syntax (Var (gensym 'x) ty))]))
+(define-syntax (F stx)
+  (syntax-parse stx
+    [(_ (a:Farg ...) (~datum :) r:Fret
+        (~optional (~seq #:pre pre)
+                   #:defaults ([pre #'(S64 1)]))
+        (~optional (~seq #:post post)
+                   #:defaults ([post #'(S64 1)]))
+        (~optional (~seq #:return ret-lab:id)
+                   #:defaults ([ret-lab (generate-temporary)]))
+        . bs)
+     ;; XXX check a ... unique
+     (syntax/loc stx
+       (let* ([ret-lab-id (gensym 'ret-lab)]
+              [a.ref a.var] ...
+              [r.ref r.var])
+         (let-syntax ([a.x (P-expander (syntax-parser [_:id #'a.ref]))] ...)
+           (let-values
+               ([(the-post the-body)
+                 (let-syntax ([r.x (P-expander (syntax-parser [_:id #'r.ref]))])
+                   (values post
+                           (let ([the-ret (Jump ret-lab-id)])
+                             (let-syntax
+                                 ([ret-lab (S-expander (syntax-parser [(_) #'the-ret]))])
+                               (syntax-parameterize
+                                   ([current-return
+                                     (make-rename-transformer #'ret-lab)]
+                                    [current-return-var
+                                     (make-rename-transformer #'r.x)])
+                                 (S (begin . bs)))))))])
+             (IntFun (list a.arg ...) pre
+                     (Var-x r.ref) (Var-ty r.ref) the-post
+                     ret-lab-id the-body)))))]))
 
-;; XXX implement Prog
+(define-syntax (define-fun stx)
+  (syntax-parse stx
+    [(_ x:id . more)
+     #:do [(define inside-Prog? (syntax-parameter-value #'current-Prog))]
+     #:with install-in-Prog
+     (if inside-Prog?
+       (syntax/loc stx
+         (hash-set! (Program-name->fun current-Prog) (symbol->string 'x) x))
+       #'(void))
+     (quasisyntax/loc stx
+       (begin (define x #,(syntax/loc #'more (F . more)))
+              install-in-Prog))]
+    [(_ (x:id . args) . more)
+     (quasisyntax/loc stx
+       (define-fun x . #,(syntax/loc #'args (args . more))))]))
+
+;; XXX define-extern-fun
+
+(define-syntax (define-global stx)
+  (syntax-parse stx
+    [(_ (~optional (~and #:public (~bind [public? #t]))
+                   #:defaults ([public? #f]))
+        x:id (~datum :) ty (~datum :=) xi)
+     #:do [(define inside-Prog? (syntax-parameter-value #'current-Prog))]
+     #:fail-when (and (attribute public?) (not inside-Prog?))
+     "Cannot define public global outside of Prog"
+     #:with make-public
+     (and inside-Prog?
+          (syntax/loc stx
+            (hash-set! (Program-private->public current-Prog)
+                       'x (symbol->string 'x))))
+     #:with install-in-Prog
+     (if inside-Prog?
+       (syntax/loc stx
+         (begin (hash-set! (Program-globals current-Prog) 'x x)
+                make-public))
+       #'(void))
+     (syntax/loc stx
+       (begin (define x (Global (T ty) (I xi)))
+              install-in-Prog))]))
+
+(define-syntax-parameter current-Prog #f)
+(define-syntax (Prog stx)
+  (syntax-parse stx
+    [(_ pf ...)
+     (syntax/loc stx
+       (let ([the-prog (Program (make-hasheq) (make-hasheq) (make-hash))])
+         (syntax-parameterize ([current-Prog (make-rename-transformer #'the-prog)])
+           pf ...)
+         the-prog))]))
 
 (provide T P E I
          while assert! S
-         define-S-free-syntax define-S-expander)
+         define-S-free-syntax define-S-expander
+         F
+         define-fun define-global
+         Prog)
 
 ;; XXX Array Slice
--- t/fib.rkt
+++ t/fib.rkt
@@ -3,29 +3,22 @@
          adqc)
 
 (define fib-p
-  (Program
-   (hasheq) (hasheq)
-   (hasheq
-    "fib"
-    (IntFun
-     (list (Arg 'n S64T 'read-only)) (S64 1)
-     'return S64T (S64 1)
-     '_return
-     (S
-      (cond
-        [(ISLe (Read (Var 'n S64T)) (S64 1))
-         (set! (Var 'return S64T) (Read (Var 'n S64T)))]
-        [else
-         (define fib : S64T := (ConI (S64 1)))
-         (define prev : S64T := (ConI (S64 1)))
-         (define i : S64T := (ConI (S64 2)))
-         (define tmp : S64T := (UndI S64T))
-         (while (ISLt (Read i) (Read (Var 'n S64T)))
-           (set! tmp (Read fib))
-           (set! fib (IAdd (Read fib) (Read prev)))
-           (set! prev (Read tmp))
-           (set! i (IAdd (Read i) (S64 1))))
-         (set! (Var 'return S64T) (Read fib))]))))))
+  (Prog
+   (define-fun (fib [n : S64T]) : [return : S64T]
+     (cond
+       [(ISLe (Read n) (S64 1))
+        (set! return (Read n))]
+       [else
+        (define fib : S64T := (ConI (S64 1)))
+        (define prev : S64T := (ConI (S64 1)))
+        (define i : S64T := (ConI (S64 2)))
+        (define tmp : S64T := (UndI S64T))
+        (while (ISLt (Read i) (Read n))
+          (set! tmp (Read fib))
+          (set! fib (IAdd (Read fib) (Read prev)))
+          (set! prev (Read tmp))
+          (set! i (IAdd (Read i) (S64 1))))
+        (set! return (Read fib))]))))
 
 (define (run-fib n)
   (Int-val (eval-program fib-p "fib" (list (S64 n)))))
