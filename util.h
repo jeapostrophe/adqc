79f4cc08fae43049bbb2569e631d55ab644a9fa0
Conor Finegan <conor_finegan@student.uml.edu>
2019-04-05 10:00:58 -0400

    format

--- util.h
+++ util.h
@@ -5,60 +5,27 @@
 // XXX use write not printf
 #include <stdio.h>
 
-static uint8_t cstr_first_char(const char* str)
-{
-  return *str;
-}
+static uint8_t cstr_first_char(const char* str) { return *str; }
 
-static int32_t print_S8(int8_t n) 
-{
-  return printf("%hhd", n);
-}
+static int32_t print_S8(int8_t n)  { return printf("%hhd", n); }
 
-static int32_t print_U8(uint8_t n)
-{
-  return printf("%hhu", n);
-}
+static int32_t print_U8(uint8_t n) { return printf("%hhu", n); }
 
-static int32_t print_S16(int16_t n)
-{
-  return printf("%hd", n);
-}
+static int32_t print_S16(int16_t n) { return printf("%hd", n); }
 
-static int32_t print_U16(uint16_t n)
-{
-  return printf("%hu", n);
-}
+static int32_t print_U16(uint16_t n) { return printf("%hu", n); }
 
-static int32_t print_S32(int32_t n)
-{
-  return printf("%d", n);
-}
+static int32_t print_S32(int32_t n) { return printf("%d", n); }
 
-static int32_t print_U32(uint32_t n)
-{
-  return printf("%u", n);
-}
+static int32_t print_U32(uint32_t n) { return printf("%u", n); }
 
-static int32_t print_S64(int64_t n)
-{
-  return printf("%ld", n);
-}
+static int32_t print_S64(int64_t n) { return printf("%ld", n); }
 
-static int32_t print_U64(uint64_t n)
-{
-  return printf("%lu", n);
-}
+static int32_t print_U64(uint64_t n) { return printf("%lu", n); }
 
-static int32_t print_F32(float n)
-{
-  return printf("%f", (double)n);
-}
+static int32_t print_F32(float n) { return printf("%f", (double)n); }
 
-static int32_t printf_F64(double n)
-{
-  return printf("%f", n);
-}
+static int32_t printf_F64(double n) { return printf("%f", n); }
 
 #endif
 
