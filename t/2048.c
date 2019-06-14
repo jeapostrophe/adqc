130375ecc211293a8bbbeb6a2372311557906481
Conor Finegan <conor_finegan@student.uml.edu>
2019-06-14 13:17:56 -0400

    more for 2048

--- t/2048.c
+++ t/2048.c
@@ -1,20 +1,47 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 #include <signal.h>
 #include <termios.h>
+#include <time.h>
 #include <unistd.h>
 
 #include "2048.h"
 
+// XXX Some way to generate this from racket code?
+#define SIZE 4
+
+uint8_t scheme = 0;
+
+void get_color(uint8_t value, char *color, size_t length) {
+  uint8_t original[] =
+    {8,255,1,255,2,255,3,255,4,255,5,255,6,255,7,255,
+     9,0,10,0,11,0,12,0,13,0,14,0,255,0,255,0};
+  uint8_t blackwhite[] =
+    {232,255,234,255,236,255,238,255,240,255,242,255,
+     244,255,246,0,248,0,249,0,250,0,251,0,252,0,253,0,254,0,255,0};
+  uint8_t bluered[] =
+    {235,255,63,255,57,255,93,255,129,255,165,255,201,255,200,255,199,
+     255,198,255,197,255,196,255,196,255,196,255,196,255,196,255};
+  uint8_t *schemes[] = {original,blackwhite,bluered};
+  uint8_t *background = schemes[scheme]+0;
+  uint8_t *foreground = schemes[scheme]+1;
+  if (value > 0) {
+    while (value--) {
+      if (background + 2 < schemes[scheme] + sizeof(original)) {
+	background+=2;
+	foreground+=2; }}}
+  snprintf(color,length,"\033[38;5;%d;48;5;%dm",*foreground,*background); }
+
 void set_buffered_input(int32_t enable) {
-  static int32_t enabled = 0;
+  static int32_t enabled = 1;
   static struct termios old;
   struct termios term;
   if (enable && !enabled) {
     tcsetattr(STDIN_FILENO, TCSANOW, &old);
-    enabled = 1;
-  } else if (!enable && enabled) {
+    enabled = 1; }
+  else if (!enable && enabled) {
     tcgetattr(STDIN_FILENO, &term);
     old = term;
     term.c_iflag &= (~ICANON & ~ECHO);
@@ -36,20 +63,56 @@ void register_sigint() {
     perror("sigaction");
     exit(1); }}
 
+void draw_board(Board board) {
+  uint8_t x, y;
+  char color[40], reset[] = "\033[m";
+  printf("\033[H");
+  printf("2048.c %17d pts\n\n", score);
+  for (y = 0; y < SIZE; ++y) {
+    for (x = 0; x < SIZE; ++x) {
+      get_color(board[x][y], color, 40);
+      printf("%s", color);
+      printf("       ");
+      printf("%s", reset); }
+    printf("\n");
+    for (x = 0; x < SIZE; ++x) {
+      get_color(board[x][y], color, 40);
+      printf("%s", color);
+      if (board[x][y] != 0) {
+	char s[8];
+	snprintf(s, 8, "%u", (uint32_t)1 << board[x][y]);
+	uint8_t t = 7 - strlen(s);
+	printf("%*s%s%*s", (t - t / 2), "", s, (t / 2), ""); }
+      else {
+	printf("   ·   "); }
+      printf("%s", reset); }
+    for (x = 0; x < SIZE; ++x) {
+      get_color(board[x][y], color, 40);
+      printf("%s", color);
+      printf("       ");
+      printf("%s", reset); }
+    printf("\n"); }
+  printf("\n");
+  printf("        ←,↑,→,↓ or q        \n");
+  printf("\033[A"); } // one line up
+
 // XXX Generate buffer space from racket code
 uint8_t rows[4][4];
 uint8_t* board[4];
 
 int32_t main(int32_t argc, char* argv[]) {
+  srand(time(NULL));
   printf("\033[?251\033[2J");
   register_sigint();
   set_buffered_input(0);
   // XXX Do this better
   make_board(board, rows[0], rows[1], rows[2], rows[3]);
 
+  // XXX init_board(board);
+
   while (1) {
     char c = getchar();
-    // Check for getchar error
+    // XXX Check for getchar error
     step(board, c); }
 
   set_buffered_input(1);
--- t/2048.rkt
+++ t/2048.rkt
@@ -5,6 +5,7 @@
          racket/runtime-path
          racket/system)
 
+(define-runtime-path 2048-exe-path "2048")
 (define-runtime-path 2048-h-path "2048.h")
 (define-runtime-path 2048-c-path "2048.c")
 (define 2048-c (ExternSrc '() (list (path->string 2048-c-path))))
@@ -63,9 +64,50 @@
         (set! success 1))))
   (return success))
 
-(define-fun (add-random [b : Board]) : S32
-  ;; XXX Need to allocate 2-dimensional array for this? Need an
-  ;; automated way to deal with those.
+(define-extern-fun rand () : S32 #:src (ExternSrc '() '("stdlib.h")))
+
+(define-fun (add-random [board : Board]) : S32
+  (define x : U8 := 0)
+  (define y : U8 := 0)
+  (define r : U8)
+  (define len : U8 := 0)
+  (define n : U8)
+  ;; XXX In the original code, this lst1 and lst2 are just lst[SIZE*SIZE][2]
+  ;; Right now multi-dimensional arrays are painful to make - fix this.
+  (define lst1 : (array (* SIZE SIZE) U8))
+  (define lst2 : (array (* SIZE SIZE) U8))
+  ;; XXX In the original code there's a static variable checking for initialized...
+  ;; Is this necessary? It just calls srand, ideally you should only need to do
+  ;; that once in main for a program like this, right?
+  ;; XXX 'for' syntax for when you don't want the iteration variable
+  ;; to be scoped to the loop?
+  (while (< x (U8 SIZE))
+    (while (< y (U8 SIZE))
+      (when (zero? (board @ x @ y))
+        (set! (lst1 @ len) x)
+        (set! (lst2 @ len) y)
+        (+=1 len))
+      (+=1 y))
+    (+=1 x))
+  (when (> len (U8 0))
+    ;; XXX This is super ugly, and will continue to be until we have ANF
+    (define rand-ret-1 := (rand))
+    (set! r (% (rand-ret-1 : U8) len))
+    (set! x (lst1 @ r))
+    (set! y (lst2 @ r))
+    (define rand-ret-2 := (rand))
+    (set! n (add1 (/ (% (rand-ret-2 : U8) 10) 9)))
+    (set! (board @ x @ y) n))
+  (return 0))
+
+(define-fun (init-board [board : Board]) : S32
+  (for ([x := (U8 0)] (< x (U8 SIZE)) (+=1 x))
+    (for ([y := (U8 0)] (< y (U8 SIZE)) (+=1 y))
+      (set! (board @ x @ y) 0)))
+  (define void1 := (add-random board))
+  (define void2 := (add-random board))
+  (define void3 := (add-random board))
+  (set! score 0)
   (return 0))
 
 (define-fun (step [b : Board] [c : S8]) : S32
@@ -76,10 +118,8 @@
   (include-type Board)
   (include-global score)
   (include-fun "make_board" make-board)
+  (include-fun "init_board" init-board)
   (include-fun step))
-
-
-(define-runtime-path 2048-exe-path "2048")
 
 (module+ test
   (require chk)
